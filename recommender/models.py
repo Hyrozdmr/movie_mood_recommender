649b9a2bc01a052f491f9825d69d04f91a0f2688
Hyrozdmr <hayriozdemir@hotmail.co.uk>
2024-10-10 12:09:50 +0100

    movie class data

--- recommender/models.py
+++ recommender/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_lenght=200)
+    genre = models.CharField(max_lenght=100)
+    mood = models.CharField(max_lenght=100)
+
+    def __str__(self):
+        return self.title
+
+
+# This defines a new class called Movie, which inherits from models.Model. 
+# In Django, all models inherit from models.Model, 
+# which is part of Django’s ORM (Object-Relational Mapping). 
+# This allows Django to automatically handle interactions between the Python code and the database
+
+# The field is a CharField, which means it will store text data.
+# The max_length=200 specifies that the maximum number of characters for the movie title is 200. 
+# This helps prevent excessively long titles from being stored in the database.
