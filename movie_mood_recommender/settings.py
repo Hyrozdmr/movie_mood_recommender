30fadad936a6ed58179677469f90598919ffeda0
Hyrozdmr <hayriozdemir@hotmail.co.uk>
2024-10-10 12:34:40 +0100

    comma added

--- movie_mood_recommender/settings.py
+++ movie_mood_recommender/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recommender'
-    'rest_framework'
+    'recommender',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
