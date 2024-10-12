0086027572515b9d87b78cc0111f3c5650670012
Hyrozdmr <hayriozdemir@hotmail.co.uk>
2024-10-12 18:29:05 +0100

    secret-key and APII-key hardcoded , .gitignore file added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z+pvioj2whvrh8o%-86s#t)oqt@m_k&j^y8v6+onma32m#$!75'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
+TMDB_API_KEY = os.getenv('TMDB_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
